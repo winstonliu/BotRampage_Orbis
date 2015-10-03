86dea7e75e2b866c84b08815deb9bdf86e384eae
winstonliu <winstonl95@hotmail.com>
2015-10-03 13:27:14 -0400

    Removed unecessary files.

--- botplayer/PlayerAI.py
+++ botplayer/PlayerAI.py
@@ -7,6 +7,16 @@ class PlayerAI:
         # Initialize any objects or variables you need here.
         pass
 
+    def isDanger(self, gameboard, player, opponent):
+        
+        # Check for turrets
+        pass
+
+
+
     def get_move(self, gameboard, player, opponent):
         # Write your AI here.
-        return Move.NONE
+        if len(gameboard.bullets) > 0:
+            for b in gameboard.bullets:
+                print(str(b.x) + "," + str(b.y) + "\t")
+        return Move.SHOOT
