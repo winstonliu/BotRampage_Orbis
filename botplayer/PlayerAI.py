a604527e93fcc135835bf484c16cbd487f390dde
Rahman Qureshi <rahman.a.qureshi@gmail.com>
2015-10-04 00:52:28 -0400

    chasing enemy

--- botplayer/PlayerAI.py
+++ botplayer/PlayerAI.py
@@ -1,6 +1,7 @@
 import time
 
 from PythonClientAPI.libs.Game.Enums import *
+from PythonClientAPI.libs.Game.GameObjects import GameObject
 from PythonClientAPI.libs.Game.MapOutOfBoundsException import *
 from networkx import nx
 
@@ -223,7 +224,37 @@ class PlayerAI:
         if len(setx)==1:
             return True
         return False
-
+        
+    def argmin(self, mlist):
+        minval = mlist[0]
+        minindex = 0
+        for i in range(1, len(mlist)):
+            if mlist[i] < minval:
+                minval = mlist[i]
+                minindex = i
+        return minindex
+    
+    def path_to_enemy(self, gameboard, player, opponent):
+        end_nodes = list(nx.all_neighbors(self.G, (opponent.y, opponent.x)))
+        opp_front_node = None
+        if opponent.direction == Direction.UP:
+            opp_front_node = (opponent.y-1, opponent.x)
+        if opponent.direction == Direction.DOWN:
+            opp_front_node = (opponent.y+1, opponent.x)
+        if opponent.direction == Direction.RIGHT:
+            opp_front_node = (opponent.y, opponent.x+1)
+        if opponent.direction == Direction.LEFT:
+            opp_front_node = (opponent.y, opponent.x-1)
+        if opp_front_node in end_nodes:
+            end_nodes.remove(opp_front_node)
+        print((player.y, player.x))
+        for end_node in end_nodes:
+            print(end_node)
+        paths = [self.get_shortest_path(player, GameObject(end_node[1], end_node[0]), []) for end_node in end_nodes]
+        if len(paths)==0:
+            return []
+        return paths[self.argmin([len(path) for path in paths])]        
+        
     def get_move(self, gameboard, player, opponent):
         start = millitime()  
         turretFARC("##### Turn: " + str(gameboard.current_turn) + " #####")
@@ -235,56 +266,21 @@ class PlayerAI:
             dbout("graph generated!")
             self.getTurretFARC(gameboard)
             dbout("Calculated turret firing arcs")
-
-        los, path = self.opponent_is_in_los(gameboard, player, opponent)
-        if los==True:
-            print("ENEMY IN LOS")
-            mmove = self.movement_direction(path, gameboard, player)
-            if mmove == Move.FORWARD:
-                return Move.SHOOT
-            else:
-                return mmove
-        else:
-            print("ENEMY NOT IN LOS")
+            
+        path = self.path_to_enemy(gameboard, player, opponent)
+#        los, path = self.opponent_is_in_los(gameboard, player, opponent)
+#        if los==True:
+#            print("ENEMY IN LOS")
+#            mmove = self.movement_direction(path, gameboard, player)
+#            if mmove == Move.FORWARD:
+#                return Move.SHOOT
+#            else:
+#                return mmove
+#        else:
+#            print("ENEMY NOT IN LOS")
     
-        # if player.shield_count>0:
-        #     return Move.SHIELD
-    
-        #if player.laser_count>0 and self.should_fire_laser(gameboard, player, opponent):
-        #    return Move.LASER
-    
-        # path = self.get_shortest_path(player, pu, [(6,1)])
-        #path = []        
-        #try:
-            #avoidance_list = self.TilesToAvoid(gameboard, player, opponent)
-        #    if opponent.laser_count > 0:
-        #        avoidance_list.extend(self.avoid_opponent_laser(gameboard, opponent))
-            #dbout(avoidance_list)
-       
-        #    to_avoid = []
-        #    while True:
-                #path = self.closest_power_up(gameboard, player, to_avoid)
-        #        if path[1] not in avoidance_list:
-        #            break
-        #        to_avoid.append(path[1])
-        #except:
-        #    print("EXCEPTION: UNABLE TO FIND PATH")
-        #    dbout(self.G.edges())
-        #    path = []
-        
-        # Debug for printing bullet specs
-        # if len(gameboard.bullets) > 0:
-        #     for i, b in enumerate(gameboard.bullets):
-        #         dbout(str(i) + ":" + str(b.x) + "," + str(b.y) + "," + str(b.direction)+ "\t")
-        
         end = millitime()
         print("Time elapsed:" + str(end - start))
-
-        # Hardcode movements 
-        # moves = [Move.SHOOT, Move.NONE]
-        # self.i = self.i+1 if self.i<len(moves)-1 else len(moves)-1
-        # return moves[self.i]
-
         
         dbout("Attempting to Follow path: ")
         dbout(path)
